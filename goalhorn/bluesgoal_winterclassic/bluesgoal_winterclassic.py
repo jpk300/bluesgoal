32713850bf5f4c75c256ac22c83ed8f03c8db5b0
Jason Kayser <jason.kayser@gmail.com>
2026-05-21 23:48:19 -0500

    Resolved light issues, rewrote php scripts

--- goalhorn/bluesgoal_winterclassic/bluesgoal_winterclassic.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system('mpg321 /var/www/html/mp3/bluesgoal_winterclassic.mp3')
--- goalhorn/marching_in/marching_in.py
+++ goalhorn/marching_in/marching_in.py
@@ -1,2 +1,47 @@
-import os
-os.system('mpg321 /var/www/html/mp3/marching_in.mp3')
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setwarnings(False)
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BOARD)
+
+PINS = [7, 8]
+
+# Active LOW relay board
+GPIO.setup(PINS, GPIO.OUT, initial=GPIO.HIGH)
+
+try:
+
+    print("Turning relays ON")
+
+    # ON
+    GPIO.output(PINS, GPIO.LOW)
+
+    print("Starting audio")
+
+    subprocess.Popen([
+        "mpg321",
+        "/var/www/html/mp3/marching_in.mp3"
+    ])
+
+    print("Keeping relays active for 30 seconds")
+
+    time.sleep(30)
+
+    print("Turning relays OFF")
+
+    # OFF
+    GPIO.output(PINS, GPIO.HIGH)
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+
+    print("Cleaning up GPIO")
+
+    GPIO.cleanup()
