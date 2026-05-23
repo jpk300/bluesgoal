05f9dec729af1220abc67f9e2a27b0ba2ed2d8c6
Jason Kayser <jason.kayser@gmail.com>
2026-05-23 14:46:02 -0500

    Updated bottom row to ovals with white background

--- goalhorn/_bluesgoal_winterclassic.php
+++ goalhorn/_bluesgoal_winterclassic.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/_helpers.php';
 
 goalhorn_run_python_action(
     'bluesgoal_winterclassic',
-    'Winter Classic goal horn started',
+    'Winter Classic goal horn has finished',
     '/var/www/html/goalhorn/bluesgoal_winterclassic/bluesgoal_winterclassic_master.py',
     true
 );
